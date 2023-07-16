90dff0edc51b590af853e1e643529d482b087aab
Luckygadjet <120310255+Luckygadjet@users.noreply.github.com>
2023-07-16 21:47:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
 function threeSum(arr, target) {
 // write your code here
+	arr.sort();
+
+	let diff = Number.MAX_SAFE_INTEGER;
+
+	let ans = 0;
+	let n= arr.length();
+	for(let i = 0;i<arr.length()-2;i++){
+
+		let  j = i+1;
+		let k = n-1;
+
+		while(j< k && k< n){
+			let sum = arr[i] + arr[j] + arr[k];
+			if(sum === target)
+			{
+				return sum;
+			}
+			else if(Math.abs(sum - target) < diff)
+			{
+				diff = Math.abs(sum - target);
+				ans = sum;
+			}
+			else if(sum < target){
+				j++;
+			}
+			else{
+				k--;
+			}
+		}
+	}
+
+	return ans;
   
 }
 
